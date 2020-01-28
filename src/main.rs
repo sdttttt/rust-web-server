e50245c0984eef6ff3e34eb68366e27373029218
sdttttt <shiinatohka@gmail.com>
2020-01-28 21:43:28 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,17 @@ fn main() {
 
     let listener = TcpListener::bind("0.0.0.0:80").unwrap();
 
-    let pool = ThreadPool::new(5);
+   let pool = ThreadPool::new(5);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         println!("Connection !");
 
-        pool.execute(move || {
+       pool.execute(move || {
             connection_handler(stream);
         })
-
     }
-
 }
 
 fn connection_handler(mut stream: TcpStream) {
@@ -30,21 +28,30 @@ fn connection_handler(mut stream: TcpStream) {
 
     let get = b"GET / HTTP/1.1\r\n";
 
-    if buffer.starts_with(get) {
-        let contents = fs::read_to_string("hello.html").unwrap();
-
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-        let contents = fs::read_to_string("404.html").unwrap();
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
 
-        let response = format!("{}{}", status_line, contents);
+    let contents = fs::read_to_string(filename).unwrap();
 
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+    // let (status_line, file_name) = if buffer.starts_with(get) {
+    //     ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    // } else {
+    //     ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    // };
+
+    // let contents = fs::read_to_string(file_name).unwrap();
+    // println!("{}", contents);
+    // let response = format!("{} {}", status_line, contents);
+
+    //     stream.write(response.as_bytes()).unwrap();
+    //     stream.flush().unwrap();
 
 }
